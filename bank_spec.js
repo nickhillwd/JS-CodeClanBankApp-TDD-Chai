bc72ce5e9aabbb30efd6519d55b1660da04357c2
nickhillwd <nickhillwd@gmail.com>
2016-01-27 19:39:01 +0000

    account keys accepting arguemets test green

--- bank_spec.js
+++ bank_spec.js
@@ -7,16 +7,29 @@ var expect = chai.expect;
 describe('Bank', function(){
   it('should be able to have individual accounts', function(){
     expect(bank.accounts).to.exist;
-  })
+  });
 })
 
 describe('Account', function(){
+  // beforeEach(function(){
+  //   console.log('setting up account1');
+  //   var account1 = new Account("Nick", "Hill", "Personal");
+  // });
   it('should have first name, last name, type, balance', function(){
-    var account1 = new Account("Nick", "Hill", "Personal")
+    var account1 = new Account("Nick", "Hill", "Personal");
     expect(account1).to.have.all.keys(['holderFirst', 'holderLast', 'type', 'balance']);
-  })
+  });
   it('should begin with a balance of £0', function(){
-    var account1 = new Account("Nick", "Hill", "Personal")
+    var account1 = new Account("Nick", "Hill", "Personal");
     expect(account1).to.contain.keys({'balance':0})
+  });
+  it('should be able to add and account with 3 arguements', function(){
+    Account.holderFirst = function(){};
+    expect(Account).itself.to.respondTo('holderFirst');
+    Account.holderLast = function(){};
+    expect(Account).itself.to.respondTo('holderLast');
+    Account.type = function(){};
+    expect(Account).itself.to.respondTo('type');
+
   })
 })
